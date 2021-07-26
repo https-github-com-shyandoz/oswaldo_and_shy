0578a72b2669404a402212170ffa1580c20e090a
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-26 11:07:02 -0500

    Updated

--- main.js
+++ main.js
@@ -3,19 +3,41 @@
 const serverURL = `https://tinted-quixotic-tractor.glitch.me/movies`
 
 
-
 fetch(serverURL).then(response => {
     response.json().then(movies => {
         console.log(movies)
-            $('#movie-body').html(
-                `
-            <div>
-               <p>Title: ${movies[0].title}</p>
-               <p>Director: ${movies[0].director}</p>
-               <img src="${movies[0].poster}">
-            </div>
+
+        let movieFilter = '';
+
+
+        function allTheMovies(movies) {
+            for (let i = 0; i < movies.length; i++) {
+
+                movieFilter +=
+                                `
+                    <div class="container-fluid">
+                        <div class="row row-cols-1 row-cols-md-2">
+                          <div class="col mb-4">
+                            <div class="card">
+                              <img src="${movies[i].poster}" class="card-img-top" alt="movie_poster">
+                              <div class="card-body">
+                                <h5 class="card-title">Title: ${movies[i].title}</h5>
+                                <p class="card-text">Director: ${movies[i].director}</p>
+                                <p class="card-text">Plot: ${movies[i].plot}</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
 `
-            )
+            }
+            $(`#movie_body`).html(movieFilter)
+        }
+        allTheMovies(movies);
+    }) // end of .then
+
+
+
+
     })
-})
 
