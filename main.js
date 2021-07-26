d71c8770d52dd66d29602208d439ac344d7485f0
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-07-26 11:26:04 -0500

    Updated

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ fetch(serverURL).then(response => {
             for (let i = 0; i < movies.length; i++) {
 
                 movieFilter +=
-                                `
+                    `
                     <div class="container-fluid">
                         <div class="row row-cols-1 row-cols-md-2">
                           <div class="col mb-4">
@@ -23,6 +23,10 @@ fetch(serverURL).then(response => {
                               <div class="card-body">
                                 <h5 class="card-title">Title: ${movies[i].title}</h5>
                                 <p class="card-text">Director: ${movies[i].director}</p>
+                                <p class="card-text">Rating: ${movies[i].rating}</p>
+                                <p class="card-text">Year: ${movies[i].year}</p>
+                                <p class="card-text">Genre: ${movies[i].genre}</p>
+                                <p class="card-text">Actors: ${movies[i].actors}</p>
                                 <p class="card-text">Plot: ${movies[i].plot}</p>
                               </div>
                             </div>
@@ -33,11 +37,10 @@ fetch(serverURL).then(response => {
             }
             $(`#movie_body`).html(movieFilter)
         }
+
         allTheMovies(movies);
     }) // end of .then
 
 
-
-
-    })
+})
 
