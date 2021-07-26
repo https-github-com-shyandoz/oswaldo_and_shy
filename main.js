ae4c69a88bfb9a153bdd7566dd3fb33e036520c3
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-07-26 13:32:51 -0500

    updated page

--- main.js
+++ main.js
@@ -28,6 +28,10 @@ fetch(serverURL).then(response => {
                                 <p class="card-text">Genre: ${movies[i].genre}</p>
                                 <p class="card-text">Actors: ${movies[i].actors}</p>
                                 <p class="card-text">Plot: ${movies[i].plot}</p>
+                                <div>
+                                <button id="edit" type="button" class="btn btn-light">Edit</button>
+                                <button id="delete" type="button" class="btn btn-light">Delete</button>
+                                </div>
                               </div>
                             </div>
                           </div>
@@ -36,11 +40,28 @@ fetch(serverURL).then(response => {
 `
             }
             $(`#movie_body`).html(movieFilter)
-        }
 
+        }
+        movieFilter += `
+        <div id="add_button" class="card bg-light mb-3" style="max-width: 18rem;">
+              <div class="card-header">Add Movie</div>
+              <div class="card-body">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>
+               
+              </div>
+            </div>
+        `
         allTheMovies(movies);
     }) // end of .then
 
+    function addMovies(){
+        $('#add_button').click((newMovie)=>{
 
+        })
+
+    }
 })
 
